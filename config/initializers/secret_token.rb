e28725429a47b350ff4c625d3a64715a4bf6b710
Ayose <ayose@foton.es>
2010-08-17 16:13:05 +0100

    Base Rails 3 application with gems loaded

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+XiUsers::Application.config.secret_token = '4b0f98ff8fabe1b5fc5373f2704e767126019f9019af2ce49f359bbc9ab115fa7dd0b612ebf5968d9033456988ade05ff4efe06b68864ad2d6a6a1b286565dec'
