b32d97f134c2e22fce1262b21c415d9b38caa71a
Angel <wage@foton.es>
2010-08-20 13:34:46 +0100

    Profiles helpers, thumbs images configuration

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,26 @@
 module UsersHelper
+
+  def link_to_profile(user, options = {})
+    link_to(user.complete_name, user_path(user), options)
+  end
+
+  def user_photo(user, options = {})
+    size = options[:big] ? Rails.application.config.xi_users.icon_size.big : Rails.application.config.xi_users.icon_size.thumb
+    user_name = user.complete_name
+    img_opts = options[:img_opts] || {}
+    if photo = user.photo
+       type = (options[:big] ? nil : :thumb)
+       html_img = image_tag(photo.public_filename(type), img_opts.merge(:size => size, :title => user_name, :alt => user_name))
+    else
+       gender = user.gender || "male"
+       html_img = image_tag(options[:big] ? Rails.application.config.xi_users.icons.send(gender).big : Rails.application.config.xi_users.icons.send(gender).thumb , img_opts.merge(:size => size, :title => user_name, :alt => user_name))
+    end
+
+    link = options[:link]
+    if link == false
+      html_img
+    else
+      link_to(html_img, link || user_path(user), options[:link_opts])
+    end 
+  end
 end
--- lib/xi-users.rb
+++ lib/xi-users.rb
@@ -5,4 +5,16 @@ class XiUsersPlugin < Rails::Engine
   config.xi_users.recaptcha = ActiveSupport::OrderedOptions.new
   config.xi_users.recaptcha.public_key = "6LcqTwEAAAAAAGnnW3NyEMwiDXT9cuPwbgoDVDhr"
   config.xi_users.recaptcha.private_key = "6LcqTwEAAAAAAKmdyQPEqlBwhZZhstJ4hMwZQq56"
+
+  config.xi_users.icon_size = ActiveSupport::OrderedOptions.new
+  config.xi_users.icon_size.big = "90x90"
+  config.xi_users.icon_size.thumb = "36x36"
+
+  config.xi_users.icons = ActiveSupport::OrderedOptions.new
+  config.xi_users.icons.male = ActiveSupport::OrderedOptions.new
+  config.xi_users.icons.male.big = "icons/male_big_icon.gif" 
+  config.xi_users.icons.male.thumb = "icons/male_thumb_icon.gif"
+  config.xi_users.icons.female = ActiveSupport::OrderedOptions.new
+  config.xi_users.icons.female.big = "icons/female_big_icon.gif"
+  config.xi_users.icons.female.thumb = "icons/female_thumb_icon.gif"
 end
